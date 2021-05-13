3bb5fc2c4008c7150e8804ddb71b8d21a01b235c
kochan-soccer <ngtm555@icloud.com>
2021-05-14 00:13:56 +0900

    laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -19,44 +20,44 @@
       </ul>
     </div>
     @endif
-    <form action="/todo/create" method="post">
-    @csrf
-      <input type="text" name="content" >
-      <input class="create_btn btn" type="submit" value="追加">
-    </form>
+      <form class="main_form" action="/todo/create" method="post">
+      @csrf
+        <input class="main_form-text" type="text" name="content" >
+        <input class="create_btn btn" type="submit" value="追加">
+      </form>
       <table>
-        <thead>
 
+        <thead>
           <tr>
-            <th>作成日</th>
-            <th>タスク名</th>
-            <th>更新</th>
-            <th>消去</th>
+            <th class="table_big">作成日</th>
+            <th class="table_big">タスク名</th>
+            <th class="table_thead-min">更新</th>
+            <th class="table_thead-min">消去</th>
           </tr>
         </thead>
 
         <tbody>
         @foreach($blogs as $blog)
           <tr>
-            <td>
+            <td class="table_big wrapper">
               <div>{{$blog->created_at}}</div>
             </td>
 
             <form action="/todo/update/" method="post">
             @csrf
               <input type="hidden" name="id" value="{{$blog->id}}">
-              <td>
-                <div>
-                  <input type="text" name="content" value="{{$blog->content}}">
+              <td class="wrapper">
+                <div class="table_big">
+                  <input class="table_big-text" type="text" name="content" value="{{$blog->content}}">
                 </div>
               </td>
-              <td>
+              <td class="table_min wrapper">
                 <input class="update_btn btn" type="submit" value="更新">
               </td>
             </form>
 
             <form action="/todo/delete/" method="post">
-              <td>
+              <td class="table_min wrapper">
                   @csrf
                   <input type="hidden" name="id" value="{{$blog->id}}">
                   <input class="delete_btn btn" type="submit" value="消去">
